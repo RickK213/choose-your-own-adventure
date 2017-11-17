55f13d90ee8ad7972384ddbcb1a4dd5ceaf6bc6c
RickK213 <rkippert@att.net>
2017-11-16 19:37:54 -0600

    Finished editing StoryNodes

--- CYOAdventure/CYOAdventure/Game.cs
+++ CYOAdventure/CYOAdventure/Game.cs
@@ -40,14 +40,14 @@ namespace CYOAdventure
                 "You decide that Andrew is probably fine and just stayed up late last night playing Overwatch and guzzling 16oz cans of Monster Energy Drink. Right after you start up your awesome Mac and get into jamming out some code in Sublime, Alex sits down and he's obviously excited. \"Dude...I found something and you're not going to believe it.\" You look up from your code as he drops the bombshell. \"It's a secret GitHub account full of pre-written Capstone Projects.\"",
                 "Do you say:",
                 "\"Holy crap! Give me the URL!\"",
-                "\"That's cray, but I'm here to learn.I'll do my own Capstone.\"",
+                "\"That's cray, but I'm here to learn. I'll do my own Capstone.\"",
                 6,
                 7,
                 false
             );
 
             StoryNode node4 = new StoryNode(
-                "\"How was your weekend, Alicia ?\" you ask. \"I told you to make it quick.I don't have time for chit-chat. Let's just say The Milwaukee River gets real cold in the wee hours of the morning.\" Alicia responds. \"Look, Mike just got here...why don't you ask him your question.\"",
+                "\"How was your weekend, Alicia?\" you ask. \"I told you to make it quick. I don't have time for chit-chat. Let's just say the Milwaukee River gets real cold in the wee hours of the morning.\" Alicia responds.\n\n\"Look, Mike just got here...why don't you ask him your question.\"",
                 "Do you:",
                 "Ask Mike about Andrew's disappearance?",
                 "Get some coffee and ponder Alicia's response?",
@@ -60,16 +60,15 @@ namespace CYOAdventure
                 "\"I noticed Andrew isn't here, what's up with that ?\" you say. \"It's not my job to babysit Andrew.\" Alicia replies in a nervous tone. \"That guy takes a lot of risks...maybe the Monster Energy Drinks finally caught up with him.\"",
                 "Do you say:",
                 "\"That's a reach, Alicia. Is there something you're not telling me ?\"",
-                "\"I'm sure you're right.Have you seen Brit this morning ? I've got some questions about her Capstone.\"",
+                "\"I'm sure you're right. Have you seen Britt this morning? I've got some questions about her Capstone.\"",
                 10,
                 11,
                 false
             );
 
             StoryNode node6 = new StoryNode(
-                "\"No problemo, homie!\" Alex says as he opens up his admittedly mediocre Windows PC and pulls up GitHub. \"The URL is 'github.com/RickK213/sweet-sweet-treasure'-- just be sure not to share it with EVERYONE!\"\n\nWhat a morning!You walked into devCodeCamp thinking you would have to bust your butt for 12 weeks and now you've got the ultimate treasure in your Chrome bookmarks.\n\nYou win the game!",
-
-               "Do you:",
+                "\"No problemo, homie!\" Alex says as he opens up his admittedly mediocre Windows PC and pulls up GitHub. \"The URL is 'github.com/RickK213/sweet-sweet-treasure' -- just be sure not to share it with EVERYONE!\"\n\nWhat a morning! You walked into devCodeCamp thinking you would have to bust your butt for 12 weeks and now you've got the ultimate treasure in your Chrome bookmarks.\n\nYou may not have solved the mystery, but YOU WIN THE GAME!",
+                "",
                 "",
                 "",
                 0,
@@ -78,113 +77,113 @@ namespace CYOAdventure
             );
 
             StoryNode node7 = new StoryNode(
-                "",
+                "\"Suit yourself, but I'm going to use it. Writing Capstone Projects is for suckers.\" Alex says. \"If you tell anyone about it though, you're dead.\"",
                 "Do you:",
-                "",
-                "",
-                0,
-                0,
+                "Scurry over to Alicia to narc on Alex?",
+                "Put your head down and continue working on your dice game?",
+                12,
+                13,
                 false
             );
 
             StoryNode node8 = new StoryNode(
+                "\"Hey Mike...Alicia sent me to you. What's up with Andrew not being here today?\" you ask. Mike's eyes dart over to Alicia's workstation as he says \"Come over to Ward 4 with me and I'll tell you. It's a sensitive subject.\"\n\nMike has never steered you wrong before so you follow him down the hallway toward the co-working space. Suddenly, Mike wheels around and you catch a glimpse of something small and thin in his hand as he spins violently.\n\nBefore you can react you notice a large red pool forming on your shirt. \"I'm sorry. I'm sorry.\" Mike blurts out. \"Alicia made me do this to you.\" As the world gets cold and dark you finally realize that Mike has just killed you by slitting your throat with a Pokémon card.\n\nYOU LOSE THE GAME.",
                 "",
-                "Do you:",
                 "",
                 "",
                 0,
                 0,
-                false
+                true
             );
 
             StoryNode node9 = new StoryNode(
-                "",
-                "Do you:",
-                "",
-                "",
-                0,
-                0,
+                "As you stroll over to the coffee pot for some of the sweet black nectar that Zach brewed up, Britt walks in the door looking white as a ghost.\n\n\"Is Alicia here?\" she asks. \"Yeah, she's right over there. She's not in a great mood though.\n\nWith an air of mystery she asks \"Are you feeling brave today?\"",
+                "Do you say:",
+                "\"I guess I'm feeling brave...what's up ?\"",
+                "\"Not really. People be acting crazy up in this piece today.\"",
+                14,
+                15,
                 false
             );
 
             StoryNode node10 = new StoryNode(
+                "Alicia's eyes well up with tears as her body nearly goes limp. \"I killed Andrew this weekend.\" she confesses. \"I had to do it. He WOULD NOT stop harping on the fact that I'm a Mac user. He pushed me to the edge.\"\n\nWhat a morning! You woke up thinking you'd be writing some Javascript code using best practices and you ended up solving The Mystery of The Missing Instructor.\n\nYOU WIN THE GAME!",
                 "",
-                "Do you:",
                 "",
                 "",
                 0,
                 0,
-                false
+                true
             );
 
             StoryNode node11 = new StoryNode(
-                "",
-                "Do you:",
-                "",
-                "",
-                0,
-                0,
+                "\"She's where she always is,\" Alicia says, \"winning money in Foosball across the hall.\"\n\nYou traverse the hallway into the lecture room where you find Britt. She's not playing Foos, though. She's in the middle of a group of people who make a path for you to talk to her.\n\n\"There you are,\" she says, \"are you feeling brave today?\"",
+                "Do you say:",
+                "\"I guess I'm feeling brave...what's up?\"",
+                "\"Uh, not really. To be honest everyone is acting weird today.\"",
+                14,
+                15,
                 false
             );
 
             StoryNode node12 = new StoryNode(
+                "You thought you were doing the right thing as you pushed back your chair, stood up and walked over to Alicia defiantly.\n\nBefore you take two steps something hard hits you in the back of the head and you fall to the floor. As you roll over you see Alex standing above you holding his gigantic Windows laptop.\n\nYou misjudged Alex. You misjudged him badly. That is your last thought as he brings down the bulky, poorly designed laptop for the fatal blow.\n\nYOU HAVE LOST THE GAME.",
                 "",
-                "Do you:",
                 "",
                 "",
                 0,
                 0,
-                false
+                true
             );
 
             StoryNode node13 = new StoryNode(
-                "",
-                "Do you:",
-                "",
-                "",
-                0,
-                0,
+                "You feel pretty good about yourself as you continue busting out the Javascript code at record pace.\n\nA few minutes later, Britt walks over with distressed look on her face.\n\n\"I have information you will not believe. Are you feeling brave today?\"",
+                "Do you say:",
+                "\"Sure I'm feeling brave you weirdo..what's up?\"",
+                "\"People are being too weird today. I just want to write code.\"",
+                14,
+                15,
                 false
             );
 
             StoryNode node14 = new StoryNode(
-                "",
+                "\"Alicia killed Andrew.\" Britt says bluntly. \"It had something to do her Mac. We need you to talk to her.\"\n\nMind blown, you think about your options.",
                 "Do you:",
-                "",
-                "",
-                0,
-                0,
+                "Confront Alicia about Andrew's death",
+                "Call the police",
+                16,
+                17,
                 false
             );
 
             StoryNode node15 = new StoryNode(
+                "\"That's too bad,\" Britt says, \"we reward bravery here at devCodeCamp.\"\n\nBritt walks over to Mike's workstation and whispers something in his ear. Mike looks in your direction with disgust in his eyes.\n\n\"I heard you don't have the guts to help out your fellow student. I'm sorry to tell you, but that is not acceptable at dCC. You are no longer a student here. Pack up your crappy Mac and hit the bricks.\"\n\nWhat a morning! You thought you could spend the morning writing some code and fly under the radar. Because of your cowardice, you have been expelled from devCodeCamp.\n\nYOU HAVE LOST THE GAME.",
                 "",
-                "Do you:",
                 "",
                 "",
                 0,
                 0,
-                false
+                true
             );
 
             StoryNode node16 = new StoryNode(
+                "You walk over to Alicia's workstation and look her dead in the eye. \"What's up with the crazy stuff I'm hearing about you and Andrew?\" you ask.\n\nAlicia's eyes well up with tears as her body nearly goes limp. \"I killed Andrew this weekend.\" she confesses. \"I had to do it. He WOULD NOT stop harping on the fact that I'm a Mac user. He pushed me to the edge.\"\n\nWhat a morning! You woke up thinking you'd be writing some Javascript code using best practices and you ended up solving The Mystery of The Missing Instructor.\n\nYOU WIN THE GAME!",
                 "",
-                "Do you:",
                 "",
                 "",
                 0,
                 0,
-                false
+                true
             );
 
             StoryNode node17 = new StoryNode(
+                "Calling the police seemed like such a reasonable thing to do.\n\nYou pull your super sweet iPhone out of your pocket and dial \"9-1-\"\n\nBefore you can hit the last \"1,\" you hear \"Do it now, Mike.\"\n\nYou turn around and see Alicia with a wild look in her eye as she puts something small and thin in Mike's hand.\n\nMike turns toward you with a pained look in his eye and the small, thin thing flashes below your chin.\n\nAs the pool of blood grows around you, you realize that Mike has been Alicia's minion all along and, at her direction, he has just sliced your throat with a Pokémon™ card.\n\n\"Charizard was my demise,\" you think, as the world gets cold and dark. You are dead.\n\nYOU HAVE LOST THE GAME.",
                 "",
-                "Do you:",
                 "",
                 "",
                 0,
                 0,
-                false
+                true
             );
 
             List<StoryNode> storyNodes = new List<StoryNode>();
@@ -211,7 +210,7 @@ namespace CYOAdventure
         public void Start()
         {
             //Introduction. User presses any key to start
-            Console.WriteLine("Welcome to The Mystery of the Missing Instructor. A choose-your-own adventure game.\n\nThroughout the game you will have 2 options: A or B\n\nSimply type the key of your choice and you will be taken to the next part of the story.\n\nType any key to start.");
+            Console.WriteLine("Welcome to The Mystery of the Missing Instructor--a choose-your-own adventure game. The ultimate goal of the game is to solve the mystery but there are other outcomes that are considered a \"win.\"\n\nThroughout the game you will have 2 options: A or B\n\nSimply type the key of your choice and you will be taken to the next part of the story.\n\nType any key to start.");
             Console.ReadKey();
             Console.Clear();
 
